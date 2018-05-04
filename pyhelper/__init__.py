8e8e30816d41e97e41efdfe483819d3c3c9b2c04
kieat <kieat.seo@gmail.com>
2018-05-04 16:55:15 +0900

    improve

--- pyhelper/__init__.py
+++ pyhelper/__init__.py
@@ -1,5 +1,5 @@
 """
-create logger instance
+create log instance
 """
 import logging
 
@@ -7,12 +7,24 @@ logging.basicConfig()
 log = logging.getLogger()
 log.setLevel(logging.INFO)
 
+log.info("================================")
+log.info("{0} of {1} is running.".format(
+    "__init__" if __package__ == __name__ else __name__,
+    __package__
+))
+
 """
 collect packages into "pip" folder to simplify source code tree and allow Python to import packages as usual
 """
 import sys
 import pathlib
 
-current_dir = pathlib.Path(__file__).parent
-print("this is current_dir: %s" % current_dir)
-sys.path.insert(0, '%s/pip' % current_dir)
+def pipdir(filepath, pipdir):
+  current_dir = pathlib.Path(filepath).parent
+  log.debug("this is current_dir: %s" % current_dir)
+  sys.path.insert(0, '{0}/{1}'.format(current_dir,pipdir))
+
+from . import *
+
+# for attr in dir():
+#   print(attr, getattr(sys.modules[__name__],attr))
